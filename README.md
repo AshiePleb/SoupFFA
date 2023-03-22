0198b3668d0fa4edff90465c02c0538d4c340a76
AshiePleb <38188828+AshiePleb@users.noreply.github.com>
2023-03-22 03:30:07 +0000

    Fixed a small bug with the popup.

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # SoupFFA
 
-SoupFFA is a plugin designed to regenerate 3.5 hearts in Minecraft when players right-click on soup or stew items. This plugin is especially useful in Free-for-all (FFA) battles or PvP situations where players need to regenerate their health quickly during a fight.
+SoupFFA is a plugin designed to regenerate 3.5 hearts in Minecraft when players right-click on soup. This plugin is especially useful in Free-for-all (FFA) battles or PvP situations where players need to regenerate their health quickly during a fight.
 
-When a player right-clicks on a soup or stew item, the plugin will instantly regenerate 3.5 hearts of the player's health. The soup or stew item is then removed from the player's inventory, preventing them from using it again until they obtain another one.
+When a player right-clicks on soup or stew item, the plugin will instantly regenerate 3.5 hearts of the player's health. The soup or stew item is then removed from the player's inventory, preventing them from using it again until they obtain another one.
 
 SoupFFA is easy to install and use and works seamlessly with other plugins on your server. This plugin will enhance the gaming experience of your players by allowing them to focus on the battle instead of worrying about their health.
 
--- src/AshiePleb/SoupFFA/Main.php
+++ src/AshiePleb/SoupFFA/Main.php
@@ -16,32 +16,27 @@ class Main extends PluginBase implements Listener {
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->enabledWorlds = $this->getConfig()->get("enabled-worlds");
         $this->configVersion = $this->getConfig()->get("config-version");
-        $this->getLogger()->info("SoupFFA Has Been Enabled");
         if ($this->configVersion !== $this->getDescription()->getVersion()) {
-            $this->getLogger()->info("You're using an outdated version of the plugin, please head over to the plugin page to download the latest version.");
+            $this->getLogger()->warning("You're using an outdated version of the plugin, please head over to the plugin page to download the latest version.");
         }
     }
 
     public function handleInteract(PlayerInteractEvent $event) {
         $player = $event->getPlayer();
         $levelName = $player->getWorld()->getFolderName();
-        if ($player->getHealth() != $player->getMaxHealth()) {
-            foreach ($this->enabledWorlds as $item) {
-                if ($levelName == $item) {
-                    if ($event->getItem()->equals(VanillaItems::MUSHROOM_STEW()) ||
-                        $event->getItem()->equals(VanillaItems::RABBIT_STEW()) ||
-                        $event->getItem()->equals(VanillaItems::BEETROOT_SOUP())) {
+        foreach ($this->enabledWorlds as $item) {
+            if ($levelName == $item) {
+                if ($event->getItem()->equals(VanillaItems::MUSHROOM_STEW()) ||
+                    $event->getItem()->equals(VanillaItems::RABBIT_STEW()) ||
+                    $event->getItem()->equals(VanillaItems::BEETROOT_SOUP())) {
+                    if ($player->getHealth() != $player->getMaxHealth()) {
                         $player->getInventory()->setItemInHand(VanillaItems::AIR());
                         $player->setHealth($player->getHealth() + 3.5);
+                    } else {
+                        $player->sendPopup(TextFormat::GREEN . "Your health is already full!");
                     }
                 }
             }
-        } else {
-            $player->sendPopup(TextFormat::GREEN . "Your health is already full!");
         }
-    }
-
-    public function onDisable(): void {
-        $this->getLogger()->info("SoupFFA Has Been Disabled");
     }
 }
